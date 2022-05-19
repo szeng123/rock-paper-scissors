eaebbc726aef07fbe27d2d9963bb317bbd0ac327
Stanley Z <szeng135@outlook.com>
2022-05-19 01:49:50 -0400

    Progress on javascript

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
 function computerPlay(choice) {
-    
+   const options = ['Rock', 'Paper', 'Scissors'];
+   choice = options[Math.floor(Math.random()*options.length)];
+   return choice;
+}
+console.log(computerPlay())
+
+function playRound(playSelection, computerSelection) {
+   playSelection = prompt('Please choose between "Rock", "Paper" and "Scissors" by typing in your option', result);
+        result = result.toLowerCase();
+        result = result[0].toUpperCase() + result.slice(1);
+
+    let computerSelection = computerPlay();
+        
+    if (result === 'Rock' && computerSelection === 'Paper') {
+        
+    } 
 }
