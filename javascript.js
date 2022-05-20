43af45c7c7ae94be90a590a4b1cdedb8afae6874
szeng123 <100456502+szeng123@users.noreply.github.com>
2022-05-20 01:24:09 -0400

    Update javascript.js
    Creation of new functions

--- javascript.js
+++ javascript.js
@@ -1,18 +1,31 @@
 function computerPlay(choice) {
-   const options = ['Rock', 'Paper', 'Scissors'];
-   choice = options[Math.floor(Math.random()*options.length)];
-   return choice;
-}
-console.log(computerPlay())
+    const options = ['Rock', 'Paper', 'Scissors'];
+    choice = options[Math.floor(Math.random()*options.length)];
+    return choice;
+ }
+ console.log(computerPlay())
+ 
+ function playRound(playerSelection, computerSelection) {
+    
+  playerSelection = prompt('Please choose between "Rock", "Paper" and "Scissors" by typing in your option'); 
+       let result = String(playerSelection).toLowerCase();
+       result = result[0].toUpperCase() + result.slice(1);
 
-function playRound(playSelection, computerSelection) {
-   playSelection = prompt('Please choose between "Rock", "Paper" and "Scissors" by typing in your option', result);
-        result = result.toLowerCase();
-        result = result[0].toUpperCase() + result.slice(1);
-
-    let computerSelection = computerPlay();
-        
-    if (result === 'Rock' && computerSelection === 'Paper') {
-        
-    } 
-}
+        if (result == 'Rock' || result == 'Paper' || result == 'Scissors') {
+            let computerSelection = computerPlay();
+            
+            if (result == computerSelection) {
+                console.log("It's a draw!");
+            } else if ((result == 'Paper' && computerSelection == 'Rock')||(result == 'Scissors' && computerSelection == 'Paper')||(result == 'Rock' && computerSelection == 'Scissors')) {
+              console.log('You win! ' + result + ' beats ' + computerSelection + '.');   
+            } else {
+                console.log('You lose! ' + computerSelection + ' beats ' + result  + '.')
+            }
+        } else {
+            console.log('PLease enter a valid option!')
+        }
+     
+         
+     
+         
+     } 
