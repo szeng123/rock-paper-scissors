defd2a83af342371d184fea774e34cfd7ff026b4
Stanley Z <szeng135@outlook.com>
2022-05-22 22:48:08 -0400

    Update synthax and reference errors in playRound()

--- javascript.js
+++ javascript.js
@@ -3,29 +3,31 @@ function computerPlay(choice) {
     choice = options[Math.floor(Math.random()*options.length)];
     return choice;
  }
- console.log(computerPlay())
  
- function playRound(playerSelection, computerSelection) {
-    
-  playerSelection = prompt('Please choose between "Rock", "Paper" and "Scissors" by typing in your option'); 
-       let result = String(playerSelection).toLowerCase();
-       result = result[0].toUpperCase() + result.slice(1);
 
-        if (result == 'Rock' || result == 'Paper' || result == 'Scissors') {
-            let computerSelection = computerPlay();
-            
-            if (result == computerSelection) {
-                console.log("It's a draw!");
-            } else if ((result == 'Paper' && computerSelection == 'Rock')||(result == 'Scissors' && computerSelection == 'Paper')||(result == 'Rock' && computerSelection == 'Scissors')) {
-              console.log('You win! ' + result + ' beats ' + computerSelection + '.');   
-            } else {
-                console.log('You lose! ' + computerSelection + ' beats ' + result  + '.')
+ function playRound(playerSelection, computerSelection) {
+
+ // playerSelection = prompt('Please choose between "Rock", "Paper" and "Scissors" by typing in your option'); //
+       let result = playerSelection.toLowerCase();
+       result = result[0].toUpperCase() + result.slice(1);
+        if (result === 'Rock' || result === 'Paper' || result === 'Scissors') {
+                if (result === computerSelection) {
+                    return "Player also chooses " + result +  " It's a draw!";
+                } else if ((result === 'Paper' && computerSelection === 'Rock')||(result === 'Scissors' && computerSelection === 'Paper')||(result === 'Rock' && computerSelection === 'Scissors')) {
+                    text
+                    return 'You win! ' + result + ' beats ' + computerSelection + '!';  
+                } else {
+                    return 'You lose! ' + computerSelection + ' beats ' + result  + '!';
             }
         } else {
             console.log('PLease enter a valid option!')
         }
-     
-         
-     
-         
      } 
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));   
+
+function game() {
+    
+}
