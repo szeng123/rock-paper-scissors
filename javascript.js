efaba6d1e862ea2021c9353d248b4f12cd920466
Stanley Z <szeng135@outlook.com>
2022-05-27 01:22:24 -0400

    Edit helper function and game function

--- javascript.js
+++ javascript.js
@@ -7,7 +7,17 @@ let startMatch = game()
 function game() {
 
     for (let i = 1; i <= numberOfRounds ; i++) {
-        playerChoice = prompt('Round ' + i.toString() + ' ! Please choose between "Rock", "Paper" and "Scissors" by typing in your option'); 
+        do {
+            console.log('Round ' + i.toString() + '~' );
+            let input = prompt('Round ' + i.toString() + '! Please choose between "Rock", "Paper" and "Scissors" by typing in your option'); 
+            let selection = input.toLowerCase();
+            playerChoice = selection[0].toUpperCase() + selection.slice(1);
+       
+            if ((playerChoice == "Rock")||(playerChoice == "Paper")||(playerChoice == "Scissors")) {
+                break;
+            }
+        } while ((playerChoice != "Rock")||(playerChoice != "Paper")||(playerChoice != "Scissors"));
+
         playRound(playerChoice, computerPlay())
         console.log('Player Score: ' +  playerVictory.toString() + ' Computer Score: ' + computerVictory.toString());
     }
@@ -26,26 +36,20 @@ function computerPlay(choice) {
     const options = ['Rock', 'Paper', 'Scissors'];
     choice = options[Math.floor(Math.random()*options.length)];
     return choice;
- }
+}
  
 
  function playRound(playerSelection, computerSelection) {
-       let result = playerSelection.toLowerCase();
-       result = result[0].toUpperCase() + result.slice(1);
-        if (result == 'Rock' || result == 'Paper' || result == 'Scissors') {
-                if (result == computerSelection) {
-                    console.log('Player chose ' + result + ', Computer chose ' + computerSelection);
-                    console.log("Player also chooses " + result +  " It's a draw!");
-                } else if ((result == 'Paper' && computerSelection == 'Rock')||(result == 'Scissors' && computerSelection == 'Paper')||(result == 'Rock' && computerSelection == 'Scissors')) {
-                    console.log('Player chose ' + result + ', Computer chose ' + computerSelection);
-                    console.log('You win! ' + result + ' beats ' + computerSelection + '!');
-                    return playerVictory++;  
-                } else {
-                    console.log('Player chose ' + result + ', Computer chose ' + computerSelection);
-                    console.log('You lose! ' + computerSelection + ' beats ' + result  + '!');
-                    return computerVictory++;
-            }
-        } else {
-            console.log('Please enter a valid option!')
-        }
-     } 
+    if (playerSelection == computerSelection) {
+            console.log('Player chose ' + playerSelection + ', Computer chose ' + computerSelection);
+            console.log("Player also chooses " + playerSelection +  " It's a draw!");
+    } else if ((playerSelection == 'Paper' && computerSelection == 'Rock')||(playerSelection == 'Scissors' && computerSelection == 'Paper')||(playerSelection == 'Rock' && computerSelection == 'Scissors')) {
+            console.log('Player chose ' + playerSelection + ', Computer chose ' + computerSelection);
+            console.log('You win! ' + playerSelection + ' beats ' + computerSelection + '!');
+            return playerVictory++;  
+    } else {
+            console.log('Player chose ' + playerSelection + ', Computer chose ' + computerSelection);
+            console.log('You lose! ' + computerSelection + ' beats ' + playerSelection  + '!');
+            return computerVictory++;
+    }
+        } 
