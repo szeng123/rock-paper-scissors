349e9158f2453a667b8389cf56eccad018d9bfac
Stanley Z <szeng135@outlook.com>
2022-06-28 00:19:58 -0400

    Revise javascript

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let playerVictory = 0;
 let computerVictory = 0;
+let roundNumber = 1;
 
 //Get selections from button clicks
 let rockSelection = document.getElementById('rock-button');
@@ -16,55 +17,54 @@ paperSelection.addEventListener('click', function() {
 scissorsSelection.addEventListener('click', function() {
     playRound('scissors', computerPlay())
 });
+
+let roundText = document.getElementById('game-text-dialogue');
+let button = document.getElementsByClassName('selector');
+
+let gameResult = document.getElementById('game-text-result');
+let playerScore = document.getElementById('score-text-player');
+let computerScore = document.getElementById('score-text-computer');
+
+
 /*
-let startMatch = game()
+    do {
+        roundText.textContent = 'Round ' + roundNumber.toString() + '! Please choose between "Rock", "Paper" and "Scissors"';
+        roundNumber++;
+        // Button presses perform playRound function
+        if ((playerVictory == 5)||(computerVictory == 5)) {
+        break;
+        }
+        } while ((playerVictory < 5)||(computerVictory < 5)) */
+// Alert messages announcing winner of 5 rounds. Prompt for new game via page refresh
+        if (playerVictory == 5) {
+            console.log('Winner!');
+            /*
+            if(confirm('You are the winner of the rock paper scissors game! Play again? ')){}
+            else window.location.reload();
+            */
+       } else if (computerVictory > playerVictory) {
+            if(confirm('The computer is the winner of the rock paper scissors game! Play again? ')){}
+            else window.location.reload();}; 
 
-function game() {
-    
-        do {
-            console.log('Round ' + i.toString() + '~' );
-            let input = prompt('Round ' + i.toString() + '! Please choose between "Rock", "Paper" and "Scissors" by typing in your option'); 
-            let selection = input.toLowerCase();
-            playerChoice = selection;
-       
-            if ((playerChoice == "rock")||(playerChoice == "paper")||(playerChoice == "scissors")) {
-                break;
-            }
-        } while ((playerChoice != "rock")||(playerChoice != "paper")||(playerChoice != "scissors"));
 
-        playRound(playerChoice, computerPlay())
-        console.log('Player Score: ' +  playerVictory.toString() + ' Computer Score: ' + computerVictory.toString());
-    
-
-    if (playerVictory > computerVictory) {
-        console.log('You are the winner of the rock paper scissors game!');
-    } else if (computerVictory > playerVictory) {
-        console.log('The computer is the winner of the rock paper scissors game!');
-    } else {
-        console.log('Wow! A draw from all 5 rounds! A miracle!');
-    }
-}
-
-*/
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
     choice = options[Math.floor(Math.random()*options.length)];
     return choice;
 }
  
-
  function playRound(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-            console.log('Player chose ' + playerSelection + ', Computer chose ' + computerSelection);
-            console.log("Player also chooses " + playerSelection +  " It's a draw!");
-    } else if ((playerSelection == 'paper' && computerSelection == 'rock')||(playerSelection == 'scissors' && computerSelection == 'paper')||(playerSelection == 'rock' && computerSelection == 'scissors')) {
-            console.log('Player chose ' + playerSelection + ', Computer chose ' + computerSelection);
-            console.log('You win! ' + playerSelection + ' beats ' + computerSelection + '!');
-            return playerVictory++;  
+    if (playerSelection === computerSelection) {
+            gameResult.textContent = "Player chose " + playerSelection + ". Computer chose " + computerSelection + ". Player also chooses " + playerSelection +  ". It's a draw!";
+    } else if ((playerSelection === 'paper' && computerSelection === 'rock')||(playerSelection === 'scissors' && computerSelection === 'paper')||(playerSelection === 'rock' && computerSelection === 'scissors')) {
+            gameResult.textContent = 'Player chose ' + playerSelection + '. Computer chose ' + computerSelection + '. You win! ' + playerSelection + ' beats ' + computerSelection + '!';
+            playerVictory++;
+
+            playerScore.textContent = playerVictory;  
     } else {
-            console.log('Player chose ' + playerSelection + ', Computer chose ' + computerSelection);
-            console.log('You lose! ' + computerSelection + ' beats ' + playerSelection  + '!');
-            return computerVictory++;
+            gameResult.textContent = 'Player chose ' + playerSelection + '. Computer chose ' + computerSelection + '. You lose! ' + computerSelection + ' beats ' + playerSelection  + '!';
+            computerVictory++;
+            computerScore.textContent = computerVictory;
     }
         }
 
