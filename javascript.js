06c1bb39ec3fd49c3346899b2f95ff39eab6b045
Stanley Z <szeng135@outlook.com>
2022-07-05 00:59:24 -0400

    Change confirm to alert window, update of styles

--- javascript.js
+++ javascript.js
@@ -1,25 +1,31 @@
 let playerVictory = 0;
 let computerVictory = 0;
-let roundNumber = 1;
+let roundNumber = 2; //Start round count at 2 as text for round 1 exists before button clicks
 
 //Get selections from button clicks
 const rockSelection = document.getElementById('rock-button');
 const paperSelection = document.getElementById('paper-button');
 const scissorsSelection = document.getElementById('scissors-button');
 
+const playerBox = document.getElementById('weapon-player');
+const computerBox = document.getElementById('weapon-computer');
+
 // Use of anoymous functions to call a function with parameters
 rockSelection.addEventListener('click', function() {
     playRound('rock', computerPlay())
+    playerBox.innerHTML = "<img src = ../rock-paper-scissors/icons/rock.png>";
     gameRoundText(roundNumber)
     roundNumber++;
 });
 paperSelection.addEventListener('click', function() {
     playRound('paper', computerPlay())
+    playerBox.innerHTML = "<img src = ../rock-paper-scissors/icons/paper.png>";
     gameRoundText(roundNumber)
     roundNumber++;
 });
 scissorsSelection.addEventListener('click', function() {
     playRound('scissors', computerPlay())
+    playerBox.innerHTML = "<img src = ../rock-paper-scissors/icons/scissors.png>";
     gameRoundText(roundNumber)
     roundNumber++;
 });
@@ -35,12 +41,17 @@ const computerScore = document.getElementById('score-text-computer');
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
     choice = options[Math.floor(Math.random()*options.length)];
+    if (choice == options[0]){
+        computerBox.innerHTML = "<img src = ../rock-paper-scissors/icons/rock.png>";
+    } else if (choice == options[1]) {
+        computerBox.innerHTML = "<img src = ../rock-paper-scissors/icons/paper.png>";
+    } else  computerBox.innerHTML = "<img src = ../rock-paper-scissors/icons/scissors.png>";
     return choice;
 }
  
  function playRound(playerSelection, computerSelection) {
             if (playerSelection === computerSelection) {
-                gameResult.textContent = "Player chose " + playerSelection + ". Computer chose " + computerSelection + ". Player also chooses " + playerSelection +  ". It's a draw!";
+                gameResult.textContent = "Player chose " + playerSelection + ". Computer also chose " + computerSelection + ". It's a draw!";
             } else if ((playerSelection === 'paper' && computerSelection === 'rock')||(playerSelection === 'scissors' && computerSelection === 'paper')||(playerSelection === 'rock' && computerSelection === 'scissors')) {
                 playerWin(playerSelection,computerSelection);
             } else {
@@ -54,11 +65,15 @@ function playerWin(playerSelection,computerSelection) {
     playerVictory++;
     playerScore.textContent = playerVictory;
         if ((playerVictory == 5) && (playerVictory > computerVictory)) {
-            if(!confirm('You are the winner of the rock paper scissors game! Play again? ')){
-                document.getElementById('rock-button').disabled = true;
-                document.getElementById('paper-button').disabled = true;
-                document.getElementById('scissors-button').disabled = true;
-            } else window.location.reload();
+            playerScore.textContent = playerVictory;
+            document.getElementById('rock-button').disabled = true;
+            document.getElementById('paper-button').disabled = true;
+            document.getElementById('scissors-button').disabled = true;
+            setTimeout(function() 
+            {
+                alert('You are the winner of the rock paper scissors game! Play again? ');
+                location.reload();
+            }, 500);
         }  
 }
 
@@ -67,11 +82,16 @@ function computerWin(playerSelection,computerSelection) {
     computerVictory++;
     computerScore.textContent = computerVictory;
         if ((computerVictory == 5) && (computerVictory > playerVictory)) {
-            if(!confirm('The computer is the winner of the rock paper scissors game! Play again? ')){
-                document.getElementById('rock-button').disabled = true;
-                document.getElementById('paper-button').disabled = true;
-                document.getElementById('scissors-button').disabled = true;
-        } else window.location.reload();
+            computerScore.textContent = computerVictory;
+            document.getElementById('rock-button').disabled = true;
+            document.getElementById('paper-button').disabled = true;
+            document.getElementById('scissors-button').disabled = true;
+            setTimeout(function() 
+            {
+                alert('The computer is the winner of the rock paper scissors game! Play again? ');
+                location.reload();
+            }, 500);
+
     }
 }
 
